dfdb5c6bcd6ce53818698c03ed75add47b873aab
SirBuster33 <sirbuster333@gmail.com>
2020-10-10 15:28:02 +0200

    Finished Potentiometer class | settings.json randomly added??

--- src/Potentiometer.h
+++ src/Potentiometer.h
@@ -1,9 +1,12 @@
 #include <Arduino.h>
+#include <tuple>  // Allows to easily return multiple values
+                  // Source: https://stackoverflow.com/questions/321068/returning-multiple-values-from-a-c-function
 
 class Potentiometer{
   public:
     int potentiometerPin;
 
+  // Constructor
   Potentiometer (int potentiometerPin){
     this->potentiometerPin = potentiometerPin;
     pinMode (potentiometerPin, INPUT);
@@ -14,13 +17,20 @@ class Potentiometer{
   Serial.begin(9600);
   }*/
 
-  String printPotentiometerState(){
+  // Reads the input on the breadboard.
+  std::tuple<int, int> updateState(){
     int potentiometerValue = analogRead(potentiometerPin);
     double voltage = potentiometerValue / 4095.0 * 3.3;
-    Serial.print("The voltage is set at: ");
-    Serial.println(voltage);
-    delay(1000);
+    return std::make_tuple(potentiometerValue, voltage);
+  }
 
+  String printPotentiometerState(int potentiometerValue, int voltage){
+    String s;
+    s += "The potentiometer is set at: ";
+    s += potentiometerValue;
+    s += "\nThe voltage is set at: ";
+    s += voltage;
+    return s;
   }
 
 };
